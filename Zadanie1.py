779d9e1375ee67924be0d6cce56bcb0aaba0de02
sbiauek <s.biauek@gmail.com>
2020-04-01 22:15:12 +0200

    zadanie1

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,1 +1,22 @@
-print("Hello World")
+# ### Zadanie 1.1 | Interakcja i proste obliczenia (ok. 2 godz.)
+#
+# Napisz program, który prosi użytkownika (przez `input()`),
+# żeby podał, ile kosztuje kilo ziemniaków. Niech program policzy i wyświetli,
+# ile trzeba będzie zapłacić za pięć kilo ziemniaków.
+
+cena_ziemniakow = int(input("Podaj cenę kilograma ziemniaków: "))
+piec_kilo = cena_ziemniakow * 5
+print(f"Cena za pięć kilogramów ziemniaków to {piec_kilo}")
+#
+# Potem napisz program, który prosi użytkownika (przez `input()`),
+# żeby podał, ile kosztuje kilo ziemniaków i ile kilo chce kupić. Niech program policzy i wyświetli,
+# ile trzeba będzie zapłacić za te ziemniaki.
+#
+
+
+# Potem napisz program, który prosi użytkownika (przez `input()`), żeby podał,
+# ile kosztuje kilo ziemniaków, ile kilo ziemniaków chce kupić, ile kosztuje kilo bananów i ile
+# kilo bananów chce kupić. Niech program policzy i wyświetli, ile trzeba będzie zapłacić za te ziemniaki i
+# banany razem. I niech program sprawdzi i powie, za co trzeba będzie zapłacić więcej - za banany czy za ziemniaki.
+
+
