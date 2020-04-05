b60063dcf0b92769024d15f7553b1f141ddfabad
sbiauek <s.biauek@gmail.com>
2020-04-05 23:17:16 +0200

    zadanie1

---
+++ Zadanie 1.5.py
@@ -0,0 +1,14 @@
+# ### Zadanie 1.5 | Pole trójkąta (ok. 1 godz.)
+#
+# Program, który odczytuje trzy liczby, sprawdza czy liczby te mogą stanowić boki trójkąta
+# (np. z 2, 2 i 5 nie da się ułożyć trójkąta, prawa?), a jeśli mogą, oblicza pole powierzchni trójkąta o takich bokach.
+#
+# Wykorzystaj trzeci wzór z [listy](https://www.matemaks.pl/wzory-na-pole-trojkata.html).
+#
+# Tutaj użyj jednego z poznanych sposobów komunikacji z użytkownikiem. Pierwiastek kwadratowy to:
+#
+# ```
+# import math
+#
+# x = math.sqrt(10)
+# ```
