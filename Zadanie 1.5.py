5483021ed6af01ad3ed1cfb9c2d6f55b60f80d3b
sbiauek <s.biauek@gmail.com>
2020-04-11 19:38:09 +0200

    zadanie1

--- Zadanie 1.5.py
+++ Zadanie 1.5.py
@@ -12,3 +12,21 @@
 #
 # x = math.sqrt(10)
 # ```
+from math import sqrt
+
+
+
+print("Podaj długość boków trójkąta.")
+bok_a = int(input("Bok A: "))
+bok_b = int(input("Bok B: "))
+bok_c = int(input("Bok C: "))
+
+p = (bok_a + bok_b + bok_c) / 2
+
+pole_trojkata = sqrt(p * ((p - bok_a) * (p - bok_b) * (p - bok_c)))
+
+if bok_a + bok_b > bok_c:
+    print("Z takich boków nie da się zbudować trójkąta")
+else:
+    print(f"Pole trójkąta wynosi: {pole_trojkata} cm2")
+
