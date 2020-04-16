075060394479698996c0b2ac09c0bb76e7c3d08a
sbiauek <s.biauek@gmail.com>
2020-04-16 17:26:14 +0200

    zadanie 2.1

--- Zadanie 2.1.py
+++ Zadanie 2.1.py
@@ -6,22 +6,22 @@
 # Program pyta o wynik wielokrotnie, tak długo, aż użytkownik poda prawidłowy wynik.
 
 
-import random
 
-pierwsza_liczba = random.randrange(0, 100)
-druga_liczba = random.randrange(0, 100)
+from random import randint
+
+
+
+
+pierwsza_liczba = randint(0, 101)
+druga_liczba = randint(0, 101)
 print(f"Pierwsza liczba to: {pierwsza_liczba}")
 print(f"Druga liczba to: {druga_liczba}")
 suma = int(input("Podaj sumę powyższych liczb: "))
 suma1 = pierwsza_liczba + druga_liczba
-while suma == suma1:
-    print("Gratulację! To prawidłowy wynik!")
-    break
-if suma != suma1:
-    print(f"Próbuj dalej!") #Nie wiem jak zrobić tutaj, żeby wracało do "PODAJ SUME POWYZSZYCH LICZB
-                             #probowalem z continue, ale mi kiepsko to wyszlo
+while suma != suma1:
+    suma = int(input("Podaj sumę powyższych liczb: "))
 
-
+print("Gratulację! To prawidłowy wynik!")
 
 
 
