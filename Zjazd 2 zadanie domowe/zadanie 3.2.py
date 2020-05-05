374a3787d198a168fed6267306724acbf6c57de5
sbiauek <s.biauek@gmail.com>
2020-05-06 00:06:56 +0200

    Podzielone zadania

---
+++ Zjazd 2 zadanie domowe/zadanie 3.2.py
@@ -0,0 +1,17 @@
+# ### Zadanie 3.2 | Miesiące
+#
+# Zapytaj użytkownika o nazwę miesiąca i na tej podstawie wypisz mu ile dni na dany miesiąc.
+#
+# Logikę obliczania liczby dni wydziel do osobnej funkcji.
+#
+# **Wersja A**
+# Nie przyjmuj się lutym - zwracaj zawsze jedną wartość.
+#
+# **Wersja B (trudniejsza)**
+# Jeżeli użytkownik poda luty - zapytaj go o rok. Na tej podstawie policz czy w tym roku luty był przestępny czy nie.
+
+
+
+
+#A
+
